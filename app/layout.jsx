272597f0d4bdc64f0a995726e8996e5f44968acd
Aqdas Iftekhar <aqdasiftekhar@gmail.com>
2024-07-15 03:10:48 +0500

    Implemented Nav, NextAuth API and Implemented Google Auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,29 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
-import Nav from '@components/Nav';
-import Provider
- from '@components/Provider';
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 export const metadata = {
-    title: "Promptopia",
-    description: "Descover & Share AI Prompts"
+  title: "Promptopia",
+  description: "Descover & Share AI Prompts",
 };
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-            <main className="app">
-                <Nav/>
-                {children}
-            </main>
-        </body>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
+  );
 };
 
 export default RootLayout;
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,19 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
+
   const [providers, setProviders] = useState(null);
 
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
-  //   useEffect(() => {
-  //     const setProviders = async () => {
-  //       const response = await getProviders();
-  //       setProviders(response);
-  //     };
-  //     setProviders();
-  //   }, []);
+    useEffect(() => {
+      const setUpProviders = async () => {
+        const response = await getProviders();
+        setProviders(response);
+      };
+      setUpProviders();
+    }, []);
   return (
     <nav className="flex-between w-full mb-26 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -33,7 +34,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -42,10 +43,10 @@ const Nav = () => {
             <button type="button" onClick={signOut} className="outline_btn">
               Sign Out
             </button>
-
+{console.log(session?.user.image)}
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -71,10 +72,10 @@ const Nav = () => {
       </div>
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
