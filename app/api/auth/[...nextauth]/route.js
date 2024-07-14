8549c6b690e1c29374571df2e407d87b70b29db6
Aqdas Iftekhar <aqdasiftekhar@gmail.com>
2024-07-15 03:19:47 +0500

    Implemented NextAuth Callback, Fixed toLowerCase sytax issue

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,41 +4,42 @@ import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 const handler = nextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRETE
-        })
-    ],
-    async session({session}) {
-        const userSession = await User.findOne({
-            email: session.user.email
-        })
-        session.user.id = userSession._id.toString();
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRETE,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const userSession = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = userSession._id.toString();
 
-        return session;
+      return session;
     },
-    async signIn({profile}) {
-        try {
-            await connectToDB();
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-            //check if a user already exists
-            const userExists = await User.findOne({email: profile.email})
+        //check if a user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-            //if not, create a new user
-            if(!userExists){
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowercase(),
-                    image: profile.picture
-                })
-            }
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
-            
+        //if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
         }
-    }
-})
-export {handler as GET, handler as POST};
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
+export { handler as GET, handler as POST };
